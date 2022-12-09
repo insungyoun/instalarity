f494a1c72285a8ff9252d8e4db298de9e32314dc
insungyoun <insung.youn@mail.utoronto.ca>
2022-12-09 03:10:56 -0500

    no progress in sight

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 
 let username = 'insung_youn';
+let followerCount = 0;
+let followingCount = 0;
 
 const script = async username => {
     const browser = await puppeteer.launch({
@@ -10,9 +12,11 @@ const script = async username => {
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto(`https://www.instagram.com/${username}`, { waitUntil:"networkidle2" });
-    let followerCount = await page.evaluate(() => {
-        
-    })
-    return 
+    await page.goto(`https://www.instagram.com/${username}/`, { waitUntil:"networkidle2" });
+    await page.waitForSelector('//*[@id="mount_0_0_Z7"]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/ul/li[2]/div/span/span');
+    followerCount = page.$x('//*[@id="mount_0_0_Z7"]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/ul/li[2]/div/span/span');
+    let followingCount = page.$x("(//span[@class = '_ac2a _ac2b'])[3]");
+    console.log(followerCount);
 }
+
+script(username);
